d046b2e47e82a348c2f6cffaf0a973cfe654f00a
Jesse Turner <jesseturner@comcast.net>
2015-12-03 09:10:14 -0800

    creating jots, searching users

--- Jitter/Models/Jot.cs
+++ Jitter/Models/Jot.cs
@@ -6,13 +6,23 @@ using System.Web;
 
 namespace Jitter.Models
 {
-    public class Jot
+    public class Jot : IComparable
     {
-        public object Author { get; set; }
+        public JitterUser Author { get; set; }
+        [Required]
         public string Content { get; set; }
         public DateTime Date { get; set; }
         [Key]
         public int JotId { get; set; }
         public string Picture { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            //Cast the object passed in as a Jot because IComparable ...   ????
+            Jot other_jot = obj as Jot;
+            //Multiply by -1 to reverse the order.  CompareTo defaults to ascending.
+            //We want the jots to display descendingly.
+            return -1*(this.Date.CompareTo(other_jot.Date));
+        }
     }
 }
