36c4c6e4c0dd2ff0ef11d2279ba709b46e401bff
Jesse Turner <jesseturner@comcast.net>
2015-12-10 10:36:25 -0800

    added angular

---
+++ Jitter.Tests/Controllers/TestControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using Jitter.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jitter.Tests.Controllers
+{
+    [TestClass]
+    public class TestControllerTests
+    {
+        [TestMethod]
+        public void TestControllerCanCallGetAction()
+        {
+            TestController my_controller = new TestController();
+            string expected_output = "Hello, is it me for whom you are looking?";
+            string actual_output = my_controller.Get();
+            Assert.AreEqual(expected_output, actual_output);
+        }
+    }
+}
